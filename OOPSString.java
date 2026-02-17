9db57c8ed384e55f2533604e7ac98a54c50fc07d
unknown <tanishqsingh2213@gmail.com>
2026-02-17 21:59:19 +0530

    Initial commit: UC3 implementation using String.join

---
+++ OOPSString.java
@@ -0,0 +1,11 @@
+public class OOPSString {
+    public static void main(String[] args) {
+        System.out.println(String.join(" ","  ***** " ,"   ***** " , "  ******* " , " ******* "));
+        System.out.println(String.join(" "," *     * " , " *     * " , " *     * " , " *       "));
+        System.out.println(String.join(" "," *     * " , " *     * " , " *     * " , " *       "));
+        System.out.println(String.join(" "," *     * " , " *     * " , " ******* " , " ******* "));
+        System.out.println(String.join(" "," *     * " , " *     * " , " *       " , "       * "));
+        System.out.println(String.join(" "," *     * " , " *     * " , " *       " , " *     * "));
+        System.out.println(String.join(" ","  ***** " , "   ***** " , "  *       " , " ******* "));
+    }
+}
